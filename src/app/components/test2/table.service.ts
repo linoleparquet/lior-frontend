00fe9e25495d2e4a08db44fc660ef6ad89701795
linoleparquet <layanilino@gmail.com>
2020-06-25 15:40:39 +0200

    remove pagination

--- src/app/components/test2/table.service.ts
+++ src/app/components/test2/table.service.ts
@@ -1,7 +1,5 @@
 import { Injectable, PipeTransform } from '@angular/core';
-
 import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
-
 import { DecimalPipe, AsyncPipe } from '@angular/common';
 import { debounceTime, delay, switchMap, tap, map, filter } from 'rxjs/operators';
 import { SortColumn, SortDirection } from '../../directive/sortable.directive';
@@ -10,11 +8,6 @@ import { DoctorService } from 'app/services/doctor.service';
 import { Doctor } from 'models/doctor.model';
 import { HttpClient } from '@angular/common/http';
 
-
-interface SearchResult {
-  countries: Doctor[];
-  total: number;
-}
 
 interface State {
   searchTerm: string;
@@ -35,35 +28,32 @@ function sort(countries: Doctor[], column: SortColumn, direction: string): Docto
   }
 }
 
-function matches(country: Doctor, term: string, pipe: PipeTransform) {
+function matches(country: Doctor, term: string) {
   return country.name.toLowerCase().includes(term.toLowerCase())
 }
 @Injectable({ providedIn: 'root' })
 export class TableService {
   private _search$ = new Subject<void>();
   private _countries$ = new BehaviorSubject<Doctor[]>([]);
-  private _total$ = new BehaviorSubject<number>(0);
 
   private _state: State = {
     searchTerm: '',
     sortColumn: '',
     sortDirection: ''
   };
-  private DOCTORS: Doctor[] = DOCTORS;
+  private _DOCTORS: Doctor[] = DOCTORS;
 
   constructor(private pipe: DecimalPipe, private doctorService: DoctorService, private http: HttpClient) {
     this._search$.pipe(
       switchMap(() => this._search()),
     ).subscribe(result => {
-      this._countries$.next(result.countries);
-      this._total$.next(result.total);
+      this._countries$.next(result);
     });
 
     this._search$.next();
   }
 
   get countries$() { return this._countries$.asObservable(); }
-  get total$() { return this._total$.asObservable(); }
   get searchTerm() { return this._state.searchTerm; }
 
   set searchTerm(searchTerm: string) { this._set({ searchTerm }); }
@@ -75,17 +65,16 @@ export class TableService {
     this._search$.next();
   }
 
-  private _search(): Observable<SearchResult> {
+  private _search(): Observable<Doctor[]> {
     const { sortColumn, sortDirection, searchTerm } = this._state;
 
     // 1. sort
-    let countries = sort(this.DOCTORS, sortColumn, sortDirection);
+    let countries = sort(this._DOCTORS, sortColumn, sortDirection);
 
     // 2. filter
-    countries = countries.filter(country => matches(country, searchTerm, this.pipe));
-    const total = countries.length;
+    countries = countries.filter(country => matches(country, searchTerm));
 
-    return of({ countries, total });
+    return of(countries);
   }
 }
 
