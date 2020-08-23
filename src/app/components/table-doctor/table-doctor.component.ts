13ec77f98156ea6c4400300e9cd0d45651c2d299
linoleparquet <layanilino@gmail.com>
2020-08-23 19:08:17 +0200

    add global search to tabels

--- src/app/components/table-doctor/table-doctor.component.ts
+++ src/app/components/table-doctor/table-doctor.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Doctor } from 'models/doctor.model';
 import { Router } from '@angular/router';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-table-doctor',
@@ -11,12 +12,10 @@ export class TableDoctorComponent implements OnInit {
   @Input() title: String;
   @Input() doctors: Doctor[];
   @Input() isAddDoctorButton: boolean;
-  headElements: String[];
 
   constructor(private router: Router) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   getLastVisit(doctor: Doctor) {
     // appeler le service et recup le last visit date grace au last date id 
