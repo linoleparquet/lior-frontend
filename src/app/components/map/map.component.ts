ef4c31bd619b02f72c32818e21c526c61f818bfe
linoleparquet <layanilino@gmail.com>
2020-10-18 17:54:39 +0200

    add marker

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -19,7 +19,9 @@ export class MapComponent implements OnInit {
       id: 'mapbox/light-v9'
     }).addTo(map);
 
-    const marker = L.marker([50.6311634, 3.0599573]).bindPopup("<b>Hello world!</b><br>I am a popup.").addTo(map);
+    const marker = L.marker([50.6311634, 3.0599573]).setIcon(L.icon({
+      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
+    })).bindPopup("<b>Hello world!</b><br>I am a popup.").addTo(map);
 
 
   }
